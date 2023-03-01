dcc9332bb73f7188b7d98536b7fe05e5f9fd4535
Bela1025 <becovarga@gmail.com>
2023-03-01 09:57:37 +0100

    sad

--- add_item.php
+++ add_item.php
@@ -1,53 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="additem.css">
 	<title>Készlet felvétel</title>
 </head>
 <body>
-<nav class="navbar navbar-dark bg-primary" style="background-color: #e3f2fd;">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="dashboard.php">Tihanyi-Tb Kft.</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="dashboard.php">Főoldal</a>
-        </li>
-        <li class="nav-item">
-          
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Lehetőségek
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Elérhetőség</a></li>
-            <li><a class="dropdown-item" href="raktar.php">Raktár</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Megrendelés</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          
-        </li>
-      </ul>
-      <ul class="navbar-nav me-0 mb-2 mb-lg-0"> 
-        <li class="nav-item" >
-          <form method="POST" action="">
-            <input type="submit" name="logout" value="Kijelentkezés">
-          </form>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
 	<h1>Készlet felvétel</h1>
+<<<<<<< HEAD
 	
   <form action="additem.php" method="post">
+=======
+	<form action="add_item.php" method="post">
+>>>>>>> parent of f275d0d (Képfeltöltés)
 		<label for="item_name">Termék neve:</label>
 		<input type="text" id="item_name" name="item_name" required><br><br>
 		<label for="item_description">Termék leírása:</label>
@@ -56,12 +20,34 @@
 		<input type="number" id="item_quantity" name="item_quantity" min="1" required><br><br>
 		<label for="item_price">Egységár:</label>
 		<input type="number" id="item_price" name="item_price" min="0.01" step="0.01" required><br><br>
-    <label for="img_upload">Képfeltöltés:</label>
-    <form action='' method='POST' enctype='multipart/form-data'>
-    <input type='file' name='img_upload'><br><br>
 		<label for="location_id">Raktár:</label>
 		<select id="location_id" name="location_id" required>
 			<option value="" disabled selected>Válassz raktárat</option>
+<<<<<<< HEAD
+=======
+			
+      <?php
+			// Load database configuration and connect to database
+			require_once 'db_connection.php';
+      
+			
+			if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+    
+			// Fetch locations from the database
+			$sql = "SELECT location_id, location_name FROM locations ORDER BY location_name";
+			$result = mysqli_query($conn, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				while ($row = mysqli_fetch_assoc($result)) {
+					echo "<option value=\"{$row['location_id']}\">{$row['location_name']}</option>";
+				}
+			}
+
+			
+			
+      ?>
+>>>>>>> parent of f275d0d (Képfeltöltés)
       </select><br><br>
 		<input type="submit" value="Készlet felvétele">
 		
